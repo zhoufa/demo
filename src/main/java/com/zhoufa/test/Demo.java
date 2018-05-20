d5899fafdccf46ef2fa88a6504b55aed99f1c96f
zhoufa163537@gmail.com <zhoufa163537>
2018-05-20 17:25:41 +0800

    mac 提交git

---
+++ src/main/java/com/zhoufa/test/Demo.java
@@ -0,0 +1,16 @@
+package com.zhoufa.test;
+
+import com.zhoufa.test.entity.Soap;
+
+/**
+ * @author zhoufa
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("这个是我在Mac pro上的第一个输出内容");
+        Soap soap = new Soap();
+        System.out.println(soap);
+
+    }
+}
---
+++ src/main/java/com/zhoufa/test/entity/Soap.java
@@ -0,0 +1,11 @@
+package com.zhoufa.test.entity;
+
+/**
+ * @author zhoufa
+ */
+public class Soap {
+
+    public Soap(){
+        System.out.println("soap()");
+    }
+}
